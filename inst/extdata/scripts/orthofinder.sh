121a1b80b77de8c90f19bbff9aa1df8ec07de95b
Jianhong Ou <jianhong.ou@gmail.com>
2026-08-07 12:56:46 -0400

    update the workflow

--- inst/extdata/scripts/orthofinder.sh
+++
@@ -1,2 +0,0 @@
-for f in *fa ; do python orthofinder/bin/primary_transcript.py $f ; done
-orthofinder -a 12 -f primary_transcripts/
--- tests/runTests.R
+++
@@ -1,4 +0,0 @@
-require("geneClusterPattern") || stop("unable to load Package:geneClusterPattern")
-require('S4Vectors') || stop('unable to load S4Vectors')
-require("testthat") || stop("unable to load testthat")
-test_check("geneClusterPattern")
